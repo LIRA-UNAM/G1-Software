a7d28cc12f16a89dc6d68cbae8504fc1440f022e
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2025-10-10 17:02:38 -0600

    First draft of pumas_nav2 (copy from Tenshi's github)

--- Setup.sh
+++ Setup.sh
@@ -1,1 +1,1 @@
-sudo apt install ros-$ROS_DISTRO-rosidl-generator-dds-idl ros-$ROS_DISTRO-joy
+sudo apt install ros-$ROS_DISTRO-rosidl-generator-dds-idl ros-$ROS_DISTRO-joy ros-$ROS_DISTRO-tf2-geometry-msgs ros-$ROS_DISTRO-nav2-lifecycle-manager
